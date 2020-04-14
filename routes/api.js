1437487897ee61640f5f17bf0cbcb206e3ed6582
MaryHooker <mc020791@yahoo.com>
2020-04-14 16:27:27 -0500

    all but single boolean

--- routes/api.js
+++ routes/api.js
@@ -15,34 +15,49 @@ router.post('/',(req,res)=>{
 })
 
 //Find multiple residents by last name
-router.get('/lastName/:lastName',(req,res)=>{
+router.get('/last/:lastName',(req,res)=>{
     // res.send(`Display multiple residents by last name`)
-    
+    EntryCollection.find({lastName:req.params.lastName},(errors,results)=>{
+        errors ? res.send(errors) : res.send(results)
+    })
 })
 
 //Find single residents
 router.get('/single/:single',(req,res)=>{
     // res.send(`Display single residents`)
+
 })
 
 //Find one resident by first name
-router.get('/firstName/:firstName',(req,res)=>{
+router.get('/first/:firstName',(req,res)=>{
     // res.send(`Display resident by first name`)
+    EntryCollection.findOne({firstName:req.params.firstName},(errors,results)=>{
+        errors ? res.send(errors) : res.send(results);
+    })
 })
 
 //Display all residents
 router.get('/',(req,res)=>{
     // res.send(`Display all residents`)
+    EntryCollection.find({}, (errors,results)=>{
+        errors ? res.send(errors) : res.send(results);
+    })
 })
 
 //Update a resident by phone number
 router.put('/:phoneNumber',(req,res)=>{
     // res.send(`Updated resident`)
+    EntryCollection.findOneAndUpdate({phoneNumber:req.params.phoneNumber}, req.body, (errors,results)=>{
+        errors ? res.send(errors) : res.send(results);
+    })
 })
 
 //Delete a resident by phone number
 router.delete('/:phoneNumber',(req,res)=>{
     // res.send(`Deleted resident`)
+    EntryCollection.findOneAndDelete({phoneNumber:req.params.phoneNumber}, (errors,results)=>{
+        errors ? res.send(errors) : res.send(results);
+    })
 })
 
 
